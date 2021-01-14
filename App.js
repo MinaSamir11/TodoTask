6d0db11b58f318d0f58fdafdb67588fefdd2d2e4
MinaSamir11 <menasamer11@gmail.com>
2021-01-14 07:40:54 +0200

    install dependancy

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const TodoTask = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default TodoTask;
